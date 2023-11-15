a1854cf98a40ccb65ebc7322dc25f0806bdbb337
Petya Babukova <p_babukova@abv.bg>
2023-11-15 19:39:59 +0200

    Add game details page

--- client/src/components/game-list/game-list-item/GameListItem.jsx
+++ client/src/components/game-list/game-list-item/GameListItem.jsx
@@ -15,7 +15,7 @@ export default function GameListItem(
                 <img src={data.imageUrl} />
                 <h6>{data.category}</h6>
                 <h2>{data.title}</h2>
-                <Link to="/data._id" className="details-button">Details</Link>
+                <Link to={`/games/${data._id}`} className="details-button">Details</Link>
             </div>
 
         </div>
--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,26 +1,22 @@
-import { request } from "../lib/request";
+import * as request from "../lib/request";
 
 const baseUrl = 'http://localhost:3030/jsonstore/games';
 
 export const getAll = async () => {
-   const result = await request('GET', baseUrl);
-
-   console.log(result);
-
+   const result = await request.get(baseUrl);
+//    console.log(result);
    return Object.values(result);
 };
 
-export const create = async (gameData) => {
-    const response = await fetch(baseUrl, {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify(gameData)
-    });
-
-    const result = await response.json()
-
+export const getOne = async(gameId)=>{
+    const result = await request.get(`${baseUrl}/${gameId}`);
     return result;
 };
+
+export const create = async (gameData) => {
+    const result = await request.post(baseUrl, gameData);
+    return result;
+};
+
+
 
